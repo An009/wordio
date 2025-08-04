d21a71ed8bd674fd61db142bfd0691ae2bc4f589
developedByAn009 <95417288+An009@users.noreply.github.com>
2025-08-04 22:56:21 +0000

    fourth commit

--- src/components/modals/Defeat.jsx
+++ src/components/modals/Defeat.jsx
@@ -3,6 +3,9 @@ import { useTranslation } from 'react-i18next'
 import Modal from './Modal'
 import Button from '../common/Button'
 
+// Defeat.jsx
+// This component displays a defeat message when the player fails to guess the word within the allowed attempts
+// It shows the correct answer and provides an option to start a new game.
 function DefeatPopup() {
   const dispatch = useDispatch()
   const { answer } = useSelector((state) => state.board)
