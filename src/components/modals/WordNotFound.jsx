b5f885419d741e09f869428f0170392e1141f261
developedByAn009 <95417288+An009@users.noreply.github.com>
2025-08-05 22:52:18 +0000

    adding comments for components4

--- src/components/modals/WordNotFound.jsx
+++ src/components/modals/WordNotFound.jsx
@@ -3,6 +3,14 @@ import { useSelector, useDispatch } from 'react-redux'
 import { useTranslation } from 'react-i18next'
 import Popup from 'reactjs-popup'
 
+// WordNotFound.jsx
+// This component displays a message when the user tries to guess a word that is not found in
+// the game's dictionary. It uses Redux for state management and i18next for translations.
+// The message is shown in a popup that automatically closes after a short duration.
+// The component is designed to provide feedback to the user about their input.
+// It is a simple notification that informs the user that the word they entered does not exist in
+// the game's vocabulary.
+
 function WordNotFound() {
   const dispatch = useDispatch()
   const { unknown } = useSelector((state) => state.popups)
