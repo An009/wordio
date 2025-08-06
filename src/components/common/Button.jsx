dd506422edf54e6709bb1d6b5a643c9726971bfd
developedByAn009 <95417288+An009@users.noreply.github.com>
2025-08-06 15:35:49 +0000

    add comments 5

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,3 +1,7 @@
+// src/components/common/Button.jsx
+// this is a simple button component that can take an icon and test as props
+// and spread the rest of the props to the button element
+// it can be used in other components to create buttons with icons and text
 function Button({ icon, text, ...rest }) {
   return (
     <button {...rest}>
