01aca63d3a78c2f5f5b47eed1b4646d13e9c8453
developedByAn009 <95417288+An009@users.noreply.github.com>
2025-08-08 17:12:29 +0000

    adding more

--- src/components/modals/index.js
+++ src/components/modals/index.js
@@ -6,6 +6,10 @@ import Statistics from './Statistics'
 import Settings from './Settings'
 import Hints from '../modals/Hints'
 
+// Modals/index.js
+// this file serves as an entry point for all modal componenets in the application.
+// it imports various modal components such as win, defeat and how yo play
+// and exports them for use in other parts of the application.
 function Modals() {
   return (
     <>
