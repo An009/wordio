7cb07d9bfd975089c1849a087ec408050a95db49
developedByAn009 <95417288+An009@users.noreply.github.com>
2025-08-26 21:34:42 +0000

    clarifiying things

--- src/components/keyboard/Key.jsx
+++ src/components/keyboard/Key.jsx
@@ -1,5 +1,7 @@
 // src/components/keyboard/Key.jsx
 // this components displays a keyboard key with a letter and handles click events
+// it receives the letter to display, a className for styling, and an onClick handler as props
+// and renders a div with the letter and the provided className and onClick handler
 function Key({ letter, className, onClick }) {
   return (
     <div className={className} onClick={onClick}>
