4fdd6122d49de0017b86ae3065cb90348adea670
developedByAn009 <95417288+An009@users.noreply.github.com>
2025-09-15 22:34:38 +0000

    add comments to useThemes.jsx

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,3 +1,7 @@
+// src/hooks/useTheme.j
+// this hook manages the application's theme by applying the current theme to the document body.
+// it retrieves the current theme from the Redux store and updates the body's class accordingly.
+
 import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
