c3fba37a82259e2bea62fbf0275283bc895b1c9b
developedByAn009 <95417288+An009@users.noreply.github.com>
2025-09-15 22:27:49 +0000

    add comments to game.jsx

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,5 +1,9 @@
 import { Board, Controls, Keyboard } from '../components'
 
+// Game.jsx
+// This component serves as the main game interface, integrating the Board, Controls, and Keyboard components
+// to provide a cohesive gaming experience.
+
 function Game() {
   return (
     <main id="game-wrapper">
