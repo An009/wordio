55c8beeeed5e9ff1d4e2fa66677bf5275ef16f7a
developedByAn009 <95417288+An009@users.noreply.github.com>
2025-09-15 22:32:30 +0000

    add comments to themes.jsx

--- src/store/rematch/models/themes.js
+++ src/store/rematch/models/themes.js
@@ -1,3 +1,6 @@
+// src/store/rematch/models/themes.js
+// this file defines the initial state and reducers for the themes model in the application.
+// it sets the initial theme based on the user's system preference (dark or light mode).
 export const themes = {
   state: window?.matchMedia('(prefers-color-scheme:dark)').matches ? 'dark' : 'light',
   reducers: {
