48a5fe58050934ec69c31676589ec6e55ce741aa
developedByAn009 <95417288+An009@users.noreply.github.com>
2025-09-15 22:28:55 +0000

    add comments to language.jsx

--- src/store/rematch/models/language.js
+++ src/store/rematch/models/language.js
@@ -1,3 +1,5 @@
+// src/store/rematch/models/language.js
+// this file defines the initial state and reducers for the language model in the application.
 export const language = {
   state: 'en',
   reducers: {
